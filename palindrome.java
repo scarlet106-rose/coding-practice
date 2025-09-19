2a3299e314974bf46630a66cf4b33416dbdc8452
Scarlet Galindo <galindoscarlet33@gmail.com>
2025-09-18 22:26:55 -0600

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,6 @@
+// LeetCode #9 Palindrome Number
+// Given an integer x, return true if x is a palindrome and false otherwise. 
+
+// My initial thought process is to turn the Int into a String so I can reverse it as an array. 
+// Here is my code:
+
